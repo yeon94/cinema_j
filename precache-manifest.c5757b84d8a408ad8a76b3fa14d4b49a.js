3d8442c6b29a4751edcdd62c7d64b106de32322a
yeon94 <hjy9946@naver.com>
2020-08-28 20:45:12 +0900

    Updates

---
+++ precache-manifest.c5757b84d8a408ad8a76b3fa14d4b49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ac426c9800469d3fa6a6232496450b",
+    "url": "/cinema_j/index.html"
+  },
+  {
+    "revision": "c984c32cb114233205a0",
+    "url": "/cinema_j/static/js/2.d02f73e2.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/cinema_j/static/js/2.d02f73e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b28a6ee4e0e6b2f63c",
+    "url": "/cinema_j/static/js/main.4148c387.chunk.js"
+  },
+  {
+    "revision": "5f5b49f324403c27c6b2",
+    "url": "/cinema_j/static/js/runtime-main.9e254685.js"
+  },
+  {
+    "revision": "c758c0272fd375b5f0f82c442d427cb8",
+    "url": "/cinema_j/static/media/noPhotos.c758c027.jpg"
+  }
+]);
